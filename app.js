662a60d48636be779e5b631ed2e348aef3ad2ef1
AMIT SHEKHAR <amit.shekhar.iitbhu@gmail.com>
2016-03-27 16:26:33 +0530

    all type of api created for testing

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var bodyParser = require('body-parser');
 var mysql = require('mysql');
 var config = require('./config.json');
 var routes = require('./routes/index');
-var users = require('./routes/users');
 var api = require('./routes/api');
 
 var app = express();
@@ -50,7 +49,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 app.use('/api', api);
 
 // catch 404 and forward to error handler
--- routes/response.js
+++ routes/response.js
@@ -7,9 +7,29 @@ module.exports.internalError = {
     message : "Internal Error"
 };
 
+module.exports.badRequest = {
+    status : 400,
+    message : "Bad Parameter"
+};
+
+module.exports.unauthorized = {
+    status : 401,
+    message : "unauthorized"
+};
+
 module.exports.UserData = function(result,message){
     this.message = message;
     this.id = result[0].id;
     this.firstname = result[0].firstname;
     this.lastname = result[0].lastname;
+};
+
+module.exports.UserCreated = function(id,message){
+    this.message = message;
+    this.id = id;
+};
+
+module.exports.HeaderData = function(token,message){
+    this.message = message;
+    this.token = token;
 };
